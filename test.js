8b1371ac9e85b13a491a8964f43b43b3446339c4
Volodymyr <volodymyr.prysiazhniuk@nuance.com>
2017-07-11 11:05:49 -0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vpl on 2017-07-10.
+ */
+
+
+function calcArea() {
+    var height;
+    var width;
+    var area;
+
+    height = 15;
+    width = 25;
+    area = height * width;
+
+    alert("The area height: " + height + "w: " + width + " a: " + area );
+}
+
+
